6792d63de4e6ae9e56955b3715f209f1e63dab83
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-06-01 23:31:01 -0500

    avance logica

--- src/components/SongForm.jsx
+++ src/components/SongForm.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const SongForm = () => {
+
+const SongForm = ({handleSearch}) => {
+
+  const [form, setForm] = useState({})
+
+  const handleChange = e => {
+    setForm({
+      ...form,
+      [e.target.name]:e.target.value,
+    })
+  }
+
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    handleSearch(form)
+  }
   return (
-    <div>SongForm</div>
+    <div>
+      Form
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="song" id=""  onInput = {handleChange} />
+        <input type="submit" value="enviar" />
+      </form>
+
+    </div>
   )
 }
 
--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -4,21 +4,29 @@ import React, { useState, useEffect } from 'react';
 //! importación de componentes
 import SongDetails from './SongDetails';
 import SongForm from './SongForm';
-
+import SongLoader from './SongLoader';
 const SongSearch = () => {
   const [search, setSearch] = useState(null)
-  const [liryc, setLiryc] = useState(null)
+  const [lyric, setLyric] = useState(null)
   const [bio, setBio] = useState(null)
   const [loading, setLoading] = useState(false)
 
-
+  const handleSearch = data => {
+    console.log(data)
+    setSearch(data)
+    console.log(search)
+  }
 
   //*pendiente del uso del useEffect para traer la data
   return (
     <div>
       song search
-      <SongForm />
-      <SongDetails />
+      <SongForm handleSearch={handleSearch}/>
+      {loading ? <SongLoader /> : <SongDetails 
+        search ={search}
+        lyric = {lyric}
+        bio = {bio}
+      />}
     </div>
   )
 }
