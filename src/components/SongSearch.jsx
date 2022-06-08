c797b842fc073dbf25388524e549f2bbbacbc1a8
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-06-07 23:42:49 -0500

    peticiones get hechas

--- src/components/SongSearch.jsx
+++ src/components/SongSearch.jsx
@@ -5,11 +5,36 @@ import React, { useState, useEffect } from 'react';
 import SongDetails from './SongDetails';
 import SongForm from './SongForm';
 import SongLoader from './SongLoader';
+import { helpHttp } from './../helpers/HelpHttp';
 const SongSearch = () => {
   const [search, setSearch] = useState(null)
   const [lyric, setLyric] = useState(null)
   const [bio, setBio] = useState(null)
   const [loading, setLoading] = useState(false)
+
+
+  let help = helpHttp()
+
+  useEffect(() => {
+    if (search === null) return;
+    const getData = async (e) => {
+      const { artist, song } = search;
+      let artistUrl = `https://theaudiodb.com/api/v1/json/2/search.php?s=${artist}`
+      let songUrl = `https://api.lyrics.ovh/v1/${artist}/${song}`
+
+      setLoading(true)
+      const [artistRes, songRes] = await Promise.all([
+        help.get(artistUrl),
+        help.get(songUrl)
+      ])
+      setBio(artistRes);
+      setLyric(songRes);
+      setLoading(false)
+    }
+    getData()
+  }, [search])
+
+
 
   const handleSearch = data => {
     console.log(data)
@@ -20,11 +45,11 @@ const SongSearch = () => {
   return (
     <div>
       song search
-      <SongForm handleSearch={handleSearch}/>
-      {loading ? <SongLoader /> : <SongDetails 
-        search ={search}
-        lyric = {lyric}
-        bio = {bio}
+      <SongForm handleSearch={handleSearch} />
+      {loading ? <SongLoader /> : <SongDetails
+        search={search}
+        lyric={lyric}
+        bio={bio}
       />}
     </div>
   )
